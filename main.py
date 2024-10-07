031ce2ff2847869961f84852093d687948a6e0be
Dilek Taylı <83818961+dilektayli@users.noreply.github.com>
2024-10-07 22:23:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+from model import get_class
+
+intents = discord.Intents.default()
+intents.messages = True
+
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def kontrol(ctx):
+    if ctx.message.attachments:
+
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(f'Resmi buraya kaydettiniz: ./{attachment.filename}!')
+            await ctx.send("Sonucunuz yükleniyor...")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+
+   
+    else:
+        await ctx.send("Dosya yüklemeyi unuttun...")
+
+
+bot.run("token")
